40c3c6367f412d47094c6f3233abde6a01b65253
Aditya <105863819+Clyders@users.noreply.github.com>
2024-06-28 20:04:34 +0530

    Update enchant.js

--- dist/native/hellforge/enchant.js
+++ dist/native/hellforge/enchant.js
@@ -1,1 +1,30 @@
-...
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const structures_1 = require("../../structures");
+exports.default = new structures_1.NativeFunction({
+    name: "$enchant",
+    version: "1.0.0",
+    description: "Enchants a item and set the value",
+    unwrap: true,
+    args: [
+        {
+            name: "item_name",
+            description: "The item name for the enchant",
+            required: true,
+            type: ArgType.String,
+            rest: false,
+        },
+        {
+            name: "enchant_name",
+            description: "The enchantment name to be added in xyz item",
+            rest: false,
+            type: ArgType.Number,
+        },
+    ],
+    brackets: true,
+    execute(ctx, [description, index]) {
+        ctx.container.embed(0).setDescription("Successfully enchanted" + item_name + "by using" + enchant_name || null)
+        return this.success()
+    },
+});
+//# sourceMappingURL=enchant.js.map
