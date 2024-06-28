74da6b81c951512ffc1a3c08bb2dbfa3c1759fde
Aditya <105863819+Clyders@users.noreply.github.com>
2024-06-28 20:01:58 +0530

    Update enchant.d.ts

--- dist/native/hellforge/enchant.d.ts
+++ dist/native/hellforge/enchant.d.ts
@@ -1,1 +1,16 @@
-...
+import { ArgType, NativeFunction } from "../../structures";
+declare const _default: NativeFunction<[{
+    name: string;
+    description: string;
+    required: true;
+    type: ArgType.String;
+    rest: false;
+}, {
+    name: string;
+    description: string;
+    required: true;
+    rest: false;
+    type: ArgType.String;
+}], true>;
+export default _default;
+//# sourceMappingURL=enchant.d.ts.map
