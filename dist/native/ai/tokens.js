b2f41c4ee34b4efa22dd3577cdaf1e5ca0f546e6
Aditya <105863819+Clyders@users.noreply.github.com>
2024-07-13 19:00:32 +0530

    Update tokens.js

--- dist/native/ai/tokens.js
+++ dist/native/ai/tokens.js
@@ -1,8 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const structures_1 = require("../../structures");
-const { DataBase } = require("@tryforge/forge.db/dist/util");
-const { ForgeDB } = require("@tryforge/forge.db");
+const { DataBase } = require("@tryforge/forge.db/dist/util"); 
 
 exports.default = new structures_1.NativeFunction({
     name: "$tokens",
@@ -11,30 +10,10 @@ exports.default = new structures_1.NativeFunction({
     unwrap: true,
     async execute(ctx) {
         const name = "tokens";
-        const data = await DataBase.get({name, id: ctx.user?.id, value, type: "user" }).then((x) => x?.value);
-if (data === null || data === undefined) {
-    if (def) return this.successJSON(def);
-    else if (ForgeDB.defaults && name in ForgeDB.defaults) {
-        const defData = ForgeDB.defaults[name];
-        if (typeof defData === "object" && defData !== null && "functions" in defData) {
-            const d = defData;
-            // Run
-            const result = await Interpreter.run(
-                ctx.clone({
-                    data: d,
-                    allowTopLevelReturn: true,
-                    doNotSend: true,
-                    redirectErrorsToConsole: true,
-                })
-            );
-            return result === null ? this.stop() : this.successJSON(result);
-        } else return this.successJSON(defData);
-    }
-}
-
-return this.successJSON(data);
+        const data = await DataBase.get({ name, id: ctx.user?.id, type: "user" }).then((x) => x?.value);
+        return this.success(data);
 
 }
     },
 );
-//# sourceMappingURL=tokens.js.map
+//# sourceMappingURL=ai.js.map
