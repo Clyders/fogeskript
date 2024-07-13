36f8920b1ece94c1c091a4542e5ad87d01b186cf
Aditya <105863819+Clyders@users.noreply.github.com>
2024-07-13 19:01:59 +0530

    Update enchant.ts

--- src/native/hellforge/enchant.ts
+++ src/native/hellforge/enchant.ts
@@ -1,31 +1,49 @@
-import { ArgType, NativeFunction } from "../../structures"
-import { DataBase } from "@tryforge/forge.db/dist/util"
+import { ArgType, NativeFunction } from "../../structures";
+import { APISelectMenuOption, StringSelectMenuBuilder } from "discord.js";
 
 export default new NativeFunction({
     name: "$enchant",
     version: "1.0.0",
-    description: "Enchants a item and set the value",
+    description: "Enchants a random pickaxe/sword",
     unwrap: true,
     args: [
         {
             name: "name",
-            description: "The item name for the enchant",
+            description: "Which thing to enchant",
             required: true,
             type: ArgType.String,
             rest: false,
         },
-        {
-            name: "value",
-            description: "The enchantment name to be added in xyz item",
-            required: true,
-            type: ArgType.String,
-            rest: false,
-        },
-        ],
-        brackets: true,
-      async execute(ctx, [name, value]) {
-          await DataBase.set({ name, id: ctx.user?.id, value, type: "user" });
-          ctx.container.embed(0).setDescription("Successfully enchanted " + name + " by using " + value || null).setColor(0xff02a2);
-        return this.success()
+    ],
+    brackets: true,
+    execute(ctx, [name]) {
+        if (name === "pickaxe") {
+            const texts = ["lol", "yo", "L"];
+            const rnd = texts[Math.floor(Math.random() * texts.length)];
+            ctx.container.embed(0).setDescription("Select the Pickaxe from the menu").setColor(0xff02a2);
+
+            const menu = new StringSelectMenuBuilder()
+                .setCustomId("pic-$authorID")
+                .setDisabled(false)
+                .setPlaceholder("...")
+                .setMinValues(1)
+                .setMaxValues(1);
+
+            ctx.container.components.at(-1)?.addComponents(menu);
+
+            const comp = ctx.container.components.at(-1)?.components[0];
+
+            const data: APISelectMenuOption = {
+                label: rnd,
+                description: "bro",
+                value: "L",
+                default: false,
+            };
+
+            if (comp && "addOptions" in comp) {
+                comp.addOptions(data);
+            }
+        }
+        return this.success();
     },
-})
+});
