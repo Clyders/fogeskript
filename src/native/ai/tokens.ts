aa9def997d9f6d7f5beeadd4191162946be2e3a4
Aditya <105863819+Clyders@users.noreply.github.com>
2024-07-13 19:01:15 +0530

    Update tokens.ts

--- src/native/ai/tokens.ts
+++ src/native/ai/tokens.ts
@@ -1,36 +1,14 @@
-import { ArgType, IExtendedCompilationResult, Interpreter, NativeFunction  } from "../../structures"
+import { NativeFunction  } from "../../structures"
 import { DataBase } from "@tryforge/forge.db/dist/util"
-import { ForgeDB } from "@tryforge/forge.db"
 
 export default new NativeFunction({
     name: "$tokens",
     version: "1.0.0",
     description: "Tokens left for the $ai usage",
     unwrap: true,
-    brackets: true,
       async execute(ctx) {
-
-const data = await DataBase.get({ "tokens", id: ctx.user?.id, value, type: "user" }).then((x) => x?.value)
-        if (data === null || data === undefined) {
-            if (def) return this.successJSON(def)
-            else if (ForgeDB.defaults && name in ForgeDB.defaults) {
-                const defData = ForgeDB.defaults[name]
-                if (typeof defData === "object" && defData !== null && "functions" in (defData as IExtendedCompilationResult)) {
-                    const d = <IExtendedCompilationResult>defData
-                    // Run
-                    const result = await Interpreter.run(
-                        ctx.clone({
-                            data: d,
-                            allowTopLevelReturn: true,
-                            doNotSend: true,
-                            redirectErrorsToConsole: true,
-                        })
-                    )
-                    return result === null ? this.stop() : this.successJSON(result)
-                } else return this.successJSON(defData)
-            }
-        }
-
-        return this.successJSON("Tokens left:" + data)
+          const name = "tokens";
+         const data = await DataBase.get({ name, id: ctx.user?.id, type: "user" }).then((x) => x?.value);
+          return this.success(data);
     },
 })
