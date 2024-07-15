61f36b760440be514fb046b3462c5fe5a904201b
Aditya <105863819+Clyders@users.noreply.github.com>
2024-07-15 18:01:33 +0530

    Update ai.ts

--- src/native/ai/ai.ts
+++ src/native/ai/ai.ts
@@ -27,10 +27,10 @@ export default new NativeFunction({
           if (data2 === undefined || data2 === 0) {
             return this.success("You have no tokens left, get back tomorrow");
           } else {
-            let value: number = data2 !== undefined ? subtractNumber(Number(data2), 100) : 0;
+            let value: number = data2 !== undefined ? subtractNumber(Number(data2), 10) : 0;
             await DataBase.set({ name, id: ctx.user?.id, value, type: "user" });
 
-            const response = await fetch('https://api.kastg.xyz/api/ai/chatgptV4?prompt=' + text);
+            const response = await fetch('https://api.kastg.xyz/api/ai/chatgptV4?prompt=- Your role: You%27re a DOCKER Pro AI, you will help users to understand docker and how to use it specifically you have done master in docker - User Message:' + text);
             const data = await response.json();
 
             const responseText: string = data.result[0].response;
