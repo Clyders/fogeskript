a627c9b0949da76977553e4e562dcf12d4d4f17d
Aditya <105863819+Clyders@users.noreply.github.com>
2024-07-19 17:03:26 +0530

    Create addApplicationEmoji.ts

---
+++ src/native/interaction/addApplicationEmoji.ts
@@ -0,0 +1,34 @@
+import { ArgType, NativeFunction, Return } from "../../structures"
+
+export default new NativeFunction({
+    name: "$addApplicationEmoji",
+    version: "1.0.0",
+    description: "Add emojis in the application.",
+    unwrap: true,
+    args: [
+        {
+            name: "emoji",
+            description: "...",
+            required: true,
+            type: ArgType.Unknown,
+            rest: false,
+        },
+        ],
+        brackets: true,
+      async execute(ctx, [emoji]) {
+
+const BASE_URL = "https://discord.com/api/v10/applications";
+const BOT_TOKEN = ctx.client.token;
+        
+        const response = await fetch(`${BASE_URL}/me/emojis`, {
+        method: "POST",
+        headers: {
+            "Authorization": `Bot ${BOT_TOKEN}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ name: emoji })
+    });
+
+    return response.ok;
+    },
+})
