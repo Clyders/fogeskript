9ab352cbf9751f9efe3f1cf20b202cb981c3cc20
Aditya <105863819+Clyders@users.noreply.github.com>
2024-07-21 22:06:11 +0530

    Fixed $day

--- src/native/time/day.ts
+++ src/native/time/day.ts
@@ -3,10 +3,14 @@ import { ArgType, NativeFunction, Return } from "../../structures"
 export default new NativeFunction({
     name: "$day",
     version: "1.2.0",
-    description: "Returns current day",
+    description: "Returns current day/date",
     unwrap: true,
+    alias: {
+        "$date"
+           },
     output: ArgType.Number,
     execute: function() {
-        return this.success(new Date().getDay())
+    const date = new Date();        
+    return this.success(date.getDate());
     }
 })
