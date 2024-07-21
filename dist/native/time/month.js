49429818e046a41eeda5f2b29b4b34265f3abd4c
Aditya <105863819+Clyders@users.noreply.github.com>
2024-07-21 22:15:20 +0530

    Fixed $month

--- dist/native/time/month.js
+++ dist/native/time/month.js
@@ -8,7 +8,8 @@ exports.default = new structures_1.NativeFunction({
     unwrap: true,
     output: structures_1.ArgType.Number,
     execute: function () {
-        return this.success(new Date().getMonth());
+    const date = new Date();
+    return this.success(date.getMonth() + 1);
     }
 });
 //# sourceMappingURL=month.js.map
