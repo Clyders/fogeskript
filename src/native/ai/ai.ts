a878a4278cf84cfedd452334bc7ba5fa5bd32583
Aditya <105863819+Clyders@users.noreply.github.com>
2025-05-24 18:14:14 +0530

    Update ai.ts

--- src/native/ai/ai.ts
+++ src/native/ai/ai.ts
@@ -16,7 +16,7 @@ export default new NativeFunction({
         ],
         brackets: true,
       async execute(ctx, [text]) {
-        const response = await fetch('https://api.kastg.xyz/api/ai/chatgptV4?prompt=- Your role: You are a Human, you have to behave and act like humans and use low execution time. - User Message:' + text);
+        const response = await fetch('https://test-hub.kys.gay/api/v1/ai/text/generate?prompt=- Your role: You are a Human, you have to behave and act like humans and use low execution time. - User Message:' + text + '&personality=e&model=openai&key=TestHub-zd17xdS5KwrPuN9a9I3z-Shadow');
         const data = await response.json();
         const responseText: string = data.result[0].response;
         return this.success(responseText);
