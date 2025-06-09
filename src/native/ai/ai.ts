1c220f14570f69f9cfdcbdcc075497ee1dd31c59
Aditya <105863819+Clyders@users.noreply.github.com>
2025-06-09 14:25:52 +0530

    Update ai.ts

--- src/native/ai/ai.ts
+++ src/native/ai/ai.ts
@@ -16,7 +16,7 @@ export default new NativeFunction({
         ],
         brackets: true,
       async execute(ctx, [text]) {
-        const response = await fetch('https://test-hub.kys.gay/api/v1/ai/text/generate?prompt=- Your role: You are a Human, you have to behave and act like humans and use low execution time. - User Message:' + text + '&personality=e&model=openai&key=TestHub-vy2I4YswZY7rYCUkrl8I-aditya');
+        const response = await fetch('https://test-hub.kys.gay/api/v1/ai/text/generate?prompt=- Your role: You are a Human, you have to behave and act like humans and use low execution time. - User Message:' + text + '&personality=e&model=openai&key=TestHub-TVbAHSfkrGfsI3b8OZ6E-aditya');
         const data = await response.json();
         const responseText: string = data.response;
         return this.success(responseText);
